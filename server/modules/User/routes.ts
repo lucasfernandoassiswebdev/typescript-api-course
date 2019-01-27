6b7e55ec35165e21d48db43bee6f7c06deccf6d9
lucasaassis@outlook.com <lucasaassis@outlook.com>
2019-01-27 00:53:09 -0200

    master - adicionando classe responsável por definir as rotas de usuário

---
+++ server/modules/User/routes.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+
+class UserRoutes {
+
+    constructor() { }
+
+    index(req: Request, res: Response): void {
+
+    }
+
+    create(req: Request, res: Response): void {
+
+    }
+
+    findOne(req: Request, res: Response): void {
+
+    }
+
+    update(req: Request, res: Response): void {
+
+    }
+
+    destroy(req: Request, res: Response): void {
+
+    }
+}
+
+export default UserRoutes;
