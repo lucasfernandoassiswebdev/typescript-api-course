213a10621391afe4111300794f576fc3bce26725
lucasaassis@outlook.com <lucasaassis@outlook.com>
2019-01-26 23:32:48 -0200

    master - montando esqueleto dos testes unitários

---
+++ build/tests/unit/unit.test.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+describe('Testes Unitários do Controller', function () {
+    describe('Método create', function () {
+        it('Deve criar um novo usuário', function () {
+        });
+    });
+    describe('Método Update', function () {
+        it('Deve atualizar um usuáruo', function () {
+        });
+    });
+    describe('Método GET Users', function () {
+        it('Deve retornar uma lista com todos os usuários', function () {
+        });
+    });
+    describe('Método Delete', function () {
+        it('Deve deletar um usuário', function () {
+        });
+    });
+});
---
+++ tests/unit/unit.test.ts
@@ -0,0 +1,27 @@
+import { testDouble, expect } from './config/helpers';
+
+describe('Testes Unitários do Controller', () => {
+    describe('Método create', () => {
+        it('Deve criar um novo usuário', () => {
+
+        });
+    });
+
+    describe('Método Update', () => {
+        it('Deve atualizar um usuáruo', () => {
+
+        });
+    });
+
+    describe('Método GET Users', () => {
+        it('Deve retornar uma lista com todos os usuários', () => {
+
+        });
+    });    
+
+    describe('Método Delete', () => {
+        it('Deve deletar um usuário', () => {
+
+        });
+    });    
+});
