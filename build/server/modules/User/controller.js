5572846ca8de1deb1cda7b8a633d5b5fdda41955
lucasaassis@outlook.com <lucasaassis@outlook.com>
2019-01-29 21:24:07 -0200

    master - adicionando implementação dos métodos do controller

--- build/server/modules/User/controller.js
+++ build/server/modules/User/controller.js
@@ -1,32 +1,46 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var HTTPStatus = require("http-status");
+var service_1 = require("./service");
 var UserController = /** @class */ (function () {
     function UserController() {
+        this.UserService = new service_1.default();
     }
     UserController.prototype.getAll = function (req, res) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.getAll().then(function (data) {
+            res.status(HTTPStatus.OK).json({ payload: data });
+        }).catch(function (err) {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao buscar todos os usuários' });
         });
     };
     UserController.prototype.createUser = function (req, res) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.create(req.body).then(function (data) {
+            res.status(HTTPStatus.OK).json({ payload: data });
+        }).catch(function (err) {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao cadastrar usuário' });
         });
     };
     UserController.prototype.getById = function (req, res) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.getById(parseInt(req.params.id)).then(function (data) {
+            res.status(HTTPStatus.OK).json({ payload: data });
+        }).catch(function (err) {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao buscar usuário' });
         });
     };
     UserController.prototype.updateUser = function (req, res) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.update(parseInt(req.params.id), req.body).then(function (data) {
+            res.status(HTTPStatus.OK).json({
+                payload: data
+            });
+        }).catch(function (err) {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao atualizar usuário' });
         });
     };
     UserController.prototype.deleteUser = function (req, res) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.delete(parseInt(req.params.id)).then(function (data) {
+            res.status(HTTPStatus.OK).json({ payload: data });
+        }).catch(function (err) {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao excluir usuário' });
         });
     };
     return UserController;
--- server/modules/User/controller.ts
+++ server/modules/User/controller.ts
@@ -1,39 +1,54 @@
 import * as HTTPStatus from 'http-status';
 import { Request, Response } from 'express';
+import User from './service';
 
 class UserController {
 
-    constructor() {
+    private UserService: User;
 
+    constructor() {
+        this.UserService = new User();
     }
 
     getAll(req: Request, res: Response) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.getAll().then(data => {
+            res.status(HTTPStatus.OK).json({ payload: data });
+        }).catch(err => {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao buscar todos os usuários' });
         });
     }
 
     createUser(req: Request, res: Response) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.create(req.body).then(data => {
+            res.status(HTTPStatus.OK).json({ payload: data });
+        }).catch(err => {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao cadastrar usuário' });
         });
     }
 
     getById(req: Request, res: Response) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.getById(parseInt(req.params.id)).then(data => {
+            res.status(HTTPStatus.OK).json({ payload: data });
+        }).catch(err => {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao buscar usuário' });
         });
     }
 
     updateUser(req: Request, res: Response) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.update(parseInt(req.params.id), req.body).then(data => {
+            res.status(HTTPStatus.OK).json({
+                payload: data
+            });
+        }).catch(err => {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao atualizar usuário' });
         });
     }
 
     deleteUser(req: Request, res: Response) {
-        res.status(HTTPStatus.OK).json({
-            message: 'ok'
+        this.UserService.delete(parseInt(req.params.id)).then(data => {
+            res.status(HTTPStatus.OK).json({ payload: data });
+        }).catch(err => {
+            res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao excluir usuário' });
         });
     }
 }
