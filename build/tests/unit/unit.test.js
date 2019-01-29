ba874c4bec7ea141315c4957e9bce48f6082c721
lucasaassis@outlook.com <lucasaassis@outlook.com>
2019-01-28 23:08:21 -0200

    master - refatorando unit-tests

--- build/tests/unit/unit.test.js
+++ build/tests/unit/unit.test.js
@@ -1,20 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var helpers_1 = require("./config/helpers");
+var service_1 = require("../../server/modules/User/service");
 describe('Testes Unitários do Controller', function () {
     describe('Método create', function () {
         it('Deve criar um novo usuário', function () {
+            var novoUsuario = {
+                id: 1,
+                name: 'Novo Usuário',
+                email: 'novousuario@gmail.com',
+                password: '1234'
+            };
+            var user = new service_1.default();
+            return user.create(novoUsuario)
+                .then(function (data) {
+                //a ordem das colunas deve ser esta, pois é a ordem na qual as colunas vem do banco de dados
+                helpers_1.expect(data.dataValues).to.have.all.keys(['email', 'id', 'name', 'password', 'updatedAt', 'createdAt']);
+            });
         });
     });
-    describe('Método Update', function () {
-        it('Deve atualizar um usuáruo', function () {
-        });
-    });
-    describe('Método GET Users', function () {
-        it('Deve retornar uma lista com todos os usuários', function () {
-        });
-    });
-    describe('Método Delete', function () {
-        it('Deve deletar um usuário', function () {
-        });
-    });
+    // describe('Método Update', () => {
+    //     it('Deve atualizar um usuáruo', () => {
+    //     });
+    // });
+    // describe('Método GET Users', () => {
+    //     it('Deve retornar uma lista com todos os usuários', () => {
+    //     });
+    // });
+    // describe('Método Delete', () => {
+    //     it('Deve deletar um usuário', () => {
+    //     });
+    // });
 });
--- tests/unit/unit.test.ts
+++ tests/unit/unit.test.ts
@@ -1,9 +1,22 @@
 import { testDouble, expect } from './config/helpers';
+import User from '../../server/modules/User/service';
 
 describe('Testes Unitários do Controller', () => {
     describe('Método create', () => {
         it('Deve criar um novo usuário', () => {
+            const novoUsuario = {
+                id: 1,
+                name: 'Novo Usuário',
+                email: 'novousuario@gmail.com',
+                password: '1234'
+            }
 
+            const user = new User();
+            return user.create(novoUsuario)
+                .then(data => {
+                    //a ordem das colunas deve ser esta, pois é a ordem na qual as colunas vem do banco de dados
+                    expect(data.dataValues).to.have.all.keys(['email', 'id', 'name', 'password', 'updatedAt', 'createdAt']);
+                });
         });
     });
 
@@ -17,11 +30,11 @@ describe('Testes Unitários do Controller', () => {
         it('Deve retornar uma lista com todos os usuários', () => {
 
         });
-    });    
+    });
 
     describe('Método Delete', () => {
         it('Deve deletar um usuário', () => {
 
         });
-    });    
+    });
 });
