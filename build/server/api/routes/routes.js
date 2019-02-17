953dd2705b8dc440a7d7ca273001b5e539226b20
lucasaassis@outlook.com <lucasaassis@outlook.com>
2019-02-17 14:37:25 -0300

    master - refatorando módulo Routes

--- build/server/api/routes/routes.js
+++ build/server/api/routes/routes.js
@@ -3,22 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var routes_1 = require("../../modules/User/routes");
 var auth_1 = require("../../modules/auth/auth");
 var Routes = /** @class */ (function () {
-    function Routes(app, auth) {
+    function Routes() {
         this.router = new routes_1.default();
         this.tokenRoute = new auth_1.default();
-        this.auth = auth;
-        this.getRoutes(app);
     }
-    Routes.prototype.getRoutes = function (app) {
+    Routes.prototype.initRoutes = function (app, auth) {
         app.route('/').get(function (req, res) { return res.send('Hello, world!'); });
         app.route('/ola/:nome').get(function (req, res) { return res.send("Hello, " + req.params.nome + "!"); });
-        app.route('/api/users/all').all(this.auth.authenticate()).get(this.router.index);
-        app.route('/api/users/:id').all(this.auth.authenticate()).get(this.router.findOne);
-        app.route('/api/users/create').all(this.auth.authenticate()).post(this.router.create);
-        app.route('/api/users/:id/update').all(this.auth.authenticate()).put(this.router.update);
-        app.route('/api/users/:id/destroy').all(this.auth.authenticate()).delete(this.router.destroy);
+        app.route('/api/users/all').all(auth.authenticate()).get(this.router.index);
+        app.route('/api/users/:id').all(auth.authenticate()).get(this.router.findOne);
+        app.route('/api/users/create').all(auth.authenticate()).post(this.router.create);
+        app.route('/api/users/:id/update').all(auth.authenticate()).put(this.router.update);
+        app.route('/api/users/:id/destroy').all(auth.authenticate()).delete(this.router.destroy);
         app.route('/token').post(this.tokenRoute.auth);
     };
     return Routes;
 }());
-exports.default = Routes;
+exports.default = new Routes();
