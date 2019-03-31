a2b384c3550ace719e281220d149d4181ce4f65a
lucasaassis@outlook.com <lucasaassis@outlook.com>
2019-03-30 22:04:40 -0300

    master - recomitando projeto para remover arquivos do dist

--- dist/server/models/relations/relations.js
+++
@@ -1,5 +0,0 @@
-var Relations = function (model) {
-    model.Author.hasMany(model.Post, { foreignKey: 'authorId' });
-    model.Post.belongsTo(model.Author, { foreignKey: 'authorId' });
-};
-module.exports = Relations;
